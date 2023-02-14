e4bebbba30b68777c12c9b9f613321dcf483284b
Dan Berkeland <danberkeland@gmail.com>
2023-02-14 06:55:31 -0800

    updated api key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://jf42szabd5ceza7bxilic5mziq.appsync-api.us-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-5qgown4o3rfjzlywzc3dd5jrey"
+    "aws_appsync_apiKey": "da2-3j3eed6mfzco3p3nwmf5r4wary"
 };
 
 
